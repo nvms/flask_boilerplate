569ee83212b265c92bd165835cce8e48b9d1b058
nvms <jon@pye.rs>
2016-10-19 11:13:01 -0700

    'changes'

---
+++ app/util.py
@@ -0,0 +1,21 @@
+import os
+
+# given a path to a folder, this returns the size in bytes
+# this is a recursive function
+def get_folder_size(folder):
+    total_size = os.path.getsize(folder)
+    for item in os.listdir(folder):
+        itempath = os.path.join(folder, item)
+        if os.path.isfile(itempath):
+            total_size += os.path.getsize(itempath)
+        elif os.path.isdir(itempath):
+            total_size += get_folder_size(itempath)
+    return total_size
+
+# given byte value, this returns human-readable size
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['','K','M','G','T','P','E','Z']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Y', suffix)
